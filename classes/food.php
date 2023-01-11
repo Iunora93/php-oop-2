ab51fac51dc1941ceec0e172d78dac14ac4b0c97
Iunora93 <eleonoraruggieri22@gmail.com>
2023-01-11 16:56:12 +0100

    Modificato exception

--- classes/food.php
+++ classes/food.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . "/Category.php";
 require_once __DIR__ ."/../traits/Typologyitem.php";
+
 class Food extends Category{
     use Typologyitem;
     use Formatitem;
--- traits/TypologyItem.php
+++ traits/TypologyItem.php
@@ -1,4 +1,5 @@
 <?php 
+
 trait Typologyitem{
     public $typology;
 }
